bc249165c29dd14fcb0c04f6f5b48e6323f882a0
Jishnu-Nandhiath <jishnun789@gmail.com>
2020-12-19 23:49:56 +0530

    Replacing python codes with C++

---
+++ HackerRank/Interview preparation kit/Warm up challenges/Counting_valleys.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int steps,level = 0,valleys = 0;
+	string path;
+	
+	cin>>steps>>path;
+	
+	for(int i = 0; i<steps; i++)
+	{
+		path[i]=='U'?level+=1:level-=1;
+		if(path[i]=='U' && level == 0)
+			valleys++;
+		
+	}
+	cout<<valleys;
+}
