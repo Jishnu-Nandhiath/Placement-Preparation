236d87cbe85a53fafb1a591c01b2cb995374b184
Jishnu N <jishnun789@gmail.com>
2021-06-20 20:29:14 +0530

    Edited Some LeetCode Challenges

--- LeetCode/Blind 75/twosum.cpp
+++ LeetCode/Blind 75/twosum.cpp
@@ -1,22 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
 using namespace std;
 
 class Solution {
-public: 
+public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    	
-        for(int i=0; i<nums.size() ; i++)
+        for(int i = 0; i < nums.size() - 1; i++)
         {
-        	for(int j=i;j<nums.size();i++)
-        	{
-        		if(nums[i]+nums[j] == target)
-        		{
-        			return new int[] {i,j};
-				}
-			}
-	
-		}
-
-        
+            for(int j = i+1; j < nums.size() ; j++ )
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                return vector<int> {i,j};
+                } 
+            }           
+        }
+            throw invalid_argument("This is here for formality");
     }
 };
+
+int main()
+{
+	vector<int> out;
+	vector<int> inputVector = {1,2,3};
+	
+	Solution SolutionObject = Solution();
+	out = SolutionObject.twoSum(inputVector,5);	
+	
+	for(auto index = out.begin(); index != out.end() ; index++ )
+	{
+		cout<<*index<<",";
+	}
+	cout<<endl;
+	return 0;
+}
